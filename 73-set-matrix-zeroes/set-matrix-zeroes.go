de7dbcf7c88fb41371b7b582b5106f56cc6ae002
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-02-22 17:54:33 -0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -1,23 +1,38 @@
 func setZeroes(matrix [][]int)  {
     n := len(matrix)
     m := len(matrix[0])
-    rowZero := make([]bool, n)
-    colZero := make([]bool, m)
+    firstColZero := false
 
     for i := range n{
-        for j := range m{
+        for j:= range m{
             if matrix[i][j] == 0{
-                rowZero[i] = true
-                colZero[j] = true
+                matrix[i][0] = 0
+                if j == 0{
+                    firstColZero = true
+                } else {
+                    matrix[0][j] = 0
+                }
             }
         }
     }
 
-    for i := range n{
-        for j:= range m{
-            if rowZero[i] || colZero[j]{
+    for i := 1; i < n; i++{
+        for j:= 1; j < m; j++{
+            if matrix[i][0] == 0 || matrix[0][j] == 0{
                 matrix[i][j] = 0
             }
+        }
+    }
+
+    if matrix[0][0] == 0{
+        for j := range m{
+            matrix[0][j] = 0
+        }
+    }
+
+    if firstColZero{
+        for i := range n {
+            matrix[i][0] = 0
         }
     }
 }
