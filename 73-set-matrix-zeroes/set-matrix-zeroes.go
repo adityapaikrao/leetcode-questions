68ca09748c68455e2e744ed8fdddc7641c74cfb8
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-02-22 17:57:40 -0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -3,6 +3,10 @@ func setZeroes(matrix [][]int)  {
     m := len(matrix[0])
     firstColZero := false
 
+    // first pass: get all zeroed rows & cols flags
+    // if first el in row == 0 => entire row zero
+    // if first el in col == 0 => entire col zero (except first col)
+    // if firstColzero => first col zero
     for i := range n{
         for j:= range m{
             if matrix[i][j] == 0{
@@ -16,6 +20,7 @@ func setZeroes(matrix [][]int)  {
         }
     }
 
+    // mark all non edge elements to avoid overwriting flags
     for i := 1; i < n; i++{
         for j:= 1; j < m; j++{
             if matrix[i][0] == 0 || matrix[0][j] == 0{
@@ -24,12 +29,14 @@ func setZeroes(matrix [][]int)  {
         }
     }
 
+    // do first row first o.w firstcolzero can set first row element to zero
     if matrix[0][0] == 0{
         for j := range m{
             matrix[0][j] = 0
         }
     }
 
+    // finally
     if firstColZero{
         for i := range n {
             matrix[i][0] = 0
