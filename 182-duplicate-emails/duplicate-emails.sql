71a8d8439b6a1051041b22927f4f07421b3b17fe
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-02 22:43:45 -0500

    Time: 248 ms (18.96%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,10 +1,5 @@
 -- Write your PostgreSQL query statement below
-WITH email_counts AS (
-    SELECT email, COUNT(id) as email_count
-    FROM Person 
-    GROUP BY email
-)
-
 SELECT email AS Email
-FROM email_counts
-WHERE email_count >= 2
+FROM Person
+GROUP BY email
+having count(email) >= 2
