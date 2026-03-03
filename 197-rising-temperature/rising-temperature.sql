f54620f4982624505c2a8dccb620708289510830
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-03 14:10:41 -0500

    Time: 312 ms (12.86%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
 SELECT cw.id 
 FROM Weather AS cw
-JOIN Weather AS pw
+LEFT JOIN Weather AS pw
 ON pw.recordDate = (cw.recordDate - INTERVAL '1 days')
 WHERE cw.temperature > pw.temperature
 
