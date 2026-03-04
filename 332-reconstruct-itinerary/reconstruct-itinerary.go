1b833ee1cb7c34f7da73c678ad01aa789878ee23
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-03 23:43:12 -0500

    Time: 8 ms (9.84%) | Memory: 7.7 MB (18.03%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.go
+++ 332-reconstruct-itinerary/reconstruct-itinerary.go
@@ -2,56 +2,47 @@ import (
     "slices"
 )
 
-
-func dfs(node string, outDegree map[string]int, adj map[string]map[string]int, postOrder *[]string){
-    // recruse until no nbrs
-    if outDegree[node] == 0{
-        *postOrder = append(*postOrder, node)
-        return
-    }
-    
-    // sort nbrs to search in lexical order
-    nbrs := make([]string, 0, len(adj[node]))
-    for nbrKey := range adj[node]{
-        if adj[node][nbrKey] != 0{
-            nbrs = append(nbrs, nbrKey)
-        }
-    }
-
-    slices.Sort(nbrs)
-
-    for _, nbrKey := range nbrs{
-        if adj[node][nbrKey] != 0 {
-            adj[node][nbrKey]-- // reduce count
-            outDegree[node]-- // reduce outdegree
-
-            dfs(nbrKey, outDegree, adj, postOrder)
-        }
-    }
-
-    *postOrder = append(*postOrder, node)
-    return
-}
-
 func findItinerary(tickets [][]string) []string {
-    // create adjacency list & outDegree list
-    outDegree := make(map[string]int, 0)
-    adj := make(map[string]map[string]int, 0) // node -> {nbr:count}
+    // goal is to find a path that covers all edges once, starting from the src (essentially Euler path)
+    // guaranteed that atleast one such path exists
+    adj := make(map[string][]string, 0)
     for _, ticket := range tickets{
-        if adj[ticket[0]] == nil{
-            adj[ticket[0]] = make(map[string]int, 0)
-        }
-        adj[ticket[0]][ticket[1]]++
-        outDegree[ticket[0]]++
+        adj[ticket[0]] = append(adj[ticket[0]], ticket[1])
     }
 
-    // skip check since guaranteed at least one eulerian path with startNode JFK
-    postOrder := make([]string, 0)
-    dfs("JFK", outDegree, adj, &postOrder)
+    // pointers to mark next vertices for each node
+    nextNodes := make(map[string]int, 0)
+    
+    // sort to ensure traversal in lexical order
+    for key := range adj{
+        slices.Sort(adj[key])
+    }
 
-    // reverse post order traversal is the visit order
-    slices.Reverse(postOrder)
+    order := make([]string, 0, len(tickets))
+    // Do DFS until all edges are not visited
+    var dfs func(node string)
+    dfs = func(node string){
+        // no new nbrs
+        if nextNodes[node] == len(adj[node]){
+            order = append(order, node)
+            return
+        }
 
-    return postOrder
+        for i := range adj[node]{
+            if i < nextNodes[node]{
+                continue
+            }
+            nextNodes[node]++
+            dfs(adj[node][i])
+        }
+        order = append(order, node)
+        return
+
+    }
+
+    dfs("JFK")
+    slices.Reverse(order)
+
+    return order
 
 }
