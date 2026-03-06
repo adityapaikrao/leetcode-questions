e4ab93f637394f53a3e3dc62b6140a34e4399e1a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-06 13:22:45 -0500

    Time: 0 ms (100.00%) | Memory: 3.7 MB (99.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.go
+++ 70-climbing-stairs/climbing-stairs.go
@@ -1,11 +1,11 @@
 func climbStairs(n int) int {
-    dp := make([]int, n + 1)
-    dp[0] = 1
-    dp[1] = 1
+   if n <= 1{
+    return 1
+   }
 
-    for i := 2 ; i < n + 1; i++{
-        dp[i] = dp[i-1] + dp[i-2]
-    }
-
-    return dp[n]
+   prev1, prev2 := 1, 1
+   for range n {
+        prev1, prev2 = prev2, prev1 + prev2
+   }
+   return prev1
 }
