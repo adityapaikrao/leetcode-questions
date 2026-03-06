70ba7af2240315a92d5e046b25cf05c7d60e9e0f
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-06 13:17:48 -0500

    Time: 0 ms (100.00%) | Memory: 3.8 MB (79.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.go
+++ 70-climbing-stairs/climbing-stairs.go
@@ -1,21 +1,11 @@
-var memo = func() []int{
-    m := make([]int, 46)
-    m[0] = 1
-    m[1] = 1
-    return m
-}()
-
 func climbStairs(n int) int {
-    if memo[n] != 0{
-        return memo[n]
+    dp := make([]int, n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i := 2 ; i < n + 1; i++{
+        dp[i] = dp[i-1] + dp[i-2]
     }
 
-    // Base Case:
-    if n <= 1 {
-        return 1
-    }
-
-    // Recursive Case
-    memo[n] = climbStairs(n-2) + climbStairs(n-1)
-    return memo[n]
+    return dp[n]
 }
