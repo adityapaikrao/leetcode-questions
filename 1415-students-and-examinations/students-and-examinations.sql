c84cd48ffc02067329dfcde4b2b24e139c6f539a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-05 19:47:03 -0500

    Time: 398 ms (46.65%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,30 +1,14 @@
 -- Write your PostgreSQL query statement below
-WITH student_subjects AS (
-    SELECT *
-    FROM Students st
-    CROSS JOIN Subjects sb
-)
-
-
 SELECT 
-    ss.student_id, 
-    ss.student_name, 
-    ss.subject_name, 
+    st.student_id, 
+    st.student_name, 
+    sb.subject_name, 
     COUNT(ex.subject_name)  AS attended_exams
-FROM student_subjects ss
+FROM Students st
+CROSS JOIN Subjects sb
 LEFT JOIN Examinations ex
-ON ss.student_id = ex.student_id
-AND ss.subject_name = ex.subject_name
-GROUP BY ss.student_id, ss.student_name, ss.subject_name
+ON st.student_id = ex.student_id
+AND sb.subject_name = ex.subject_name
+GROUP BY st.student_id, st.student_name, sb.subject_name
 
--- SELECT 
---     ss.student_id, 
---     ss.student_name AS student_name_ss, 
---     ss.subject_name AS subject_name_ss,
---     ex.* 
---     -- COUNT(CASE WHEN ex.subject_name is NULL THEN 0 ELSE 1 END)
--- FROM student_subjects ss
--- LEFT JOIN Examinations ex
--- ON ss.student_id = ex.student_id
--- AND ss.subject_name = ex.subject_name
 
