273154fd5e4e83f14bf28e7091bcc716ff447f2f
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-07 15:21:43 -0500

    Time: 231 ms (86.54%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,18 +1,17 @@
 -- Write your PostgreSQL query statement below
-WITH user_confirmations AS (
-    SELECT 
-        sg.user_id, 
-        SUM(CASE WHEN cnf.action = 'confirmed' THEN 1.0 ELSE 0.0 END) AS user_confirmations,
-        SUM(CASE WHEN cnf.action = 'timeout' THEN 1.0 ELSE 0.0 END) AS user_timeouts
-        FROM Signups sg
-        LEFT JOIN Confirmations cnf
-        ON sg.user_id = cnf.user_id
-        GROUP BY sg.user_id
-)
-
+-- WITH user_confirmations AS (
 SELECT 
-    user_id, 
-    ROUND((CASE WHEN user_timeouts = 0 AND user_confirmations = 0 THEN 0 ELSE user_confirmations/(user_timeouts + user_confirmations) END), 2) AS confirmation_rate
+    sg.user_id, 
+    ROUND(AVG(CASE WHEN cnf.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
+    FROM Signups sg
+    LEFT JOIN Confirmations cnf
+    ON sg.user_id = cnf.user_id
+    GROUP BY sg.user_id
+-- )
 
-    FROM user_confirmations
+-- SELECT 
+--     user_id, 
+--     ROUND((CASE WHEN user_timeouts = 0 AND user_confirmations = 0 THEN 0 ELSE user_confirmations/(user_timeouts + user_confirmations) END), 2) AS confirmation_rate
+
+--     FROM user_confirmations
     
