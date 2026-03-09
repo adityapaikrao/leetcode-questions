bf8bad8e9d0bda36003373b0956f9d9a1b511dfe
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-09 11:29:46 -0400

    Time: 10 ms (80.21%) | Memory: 8.3 MB (92.03%) - LeetSync

---
+++ 322-coin-change/coin-change.go
@@ -0,0 +1,34 @@
+import (
+    "math"
+)
+
+/*
+[1, 2, 5] 11
+
+[0, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+*/
+
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount + 1)
+    for i := range dp{
+        dp[i] = math.MaxInt
+    }
+    dp[0] = 0
+
+    for i := 1; i <= amount; i++{
+        for _, coin := range coins{
+            if coin > i{
+                continue
+            }
+            if dp[i - coin] != math.MaxInt {
+                dp[i] = min(dp[i - coin] + 1, dp[i])
+            }
+        }
+    }
+
+    if dp[amount] == math.MaxInt{
+        return -1
+    }
+
+    return dp[amount]
+}
