2250afffab15ef43a583ae409eacc8b334d4ea8e
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-15 12:00:58 -0400

    Time: 0 ms (100.00%) | Memory: 3.9 MB (91.43%) - LeetSync

--- 97-interleaving-string/interleaving-string.go
+++ 97-interleaving-string/interleaving-string.go
@@ -1,9 +1,9 @@
-func boolToInt(b bool) int {
-    if b {
-        return 1
-    }
-    return 0
-}
+// func boolToInt(b bool) int {
+//     if b {
+//         return 1
+//     }
+//     return 0
+// }
 
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
@@ -11,45 +11,75 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
         return false
     }
 
-    memo := make([][]int, len(s1)+1)
-    for i := range len(s1) + 1 {
-        memo[i] = make([]int, len(s2)+1)
-        for j := range memo[i] {
-            memo[i][j] = -1
+    // memo := make([][]int, len(s1)+1)
+    // for i := range len(s1) + 1 {
+    //     memo[i] = make([]int, len(s2)+1)
+    //     for j := range memo[i] {
+    //         memo[i][j] = -1
+    //     }
+    // }
+
+    // var solve func(i, j int) bool
+    // solve = func(i, j int) bool {
+    //     if memo[i][j] != -1 {
+    //         return memo[i][j] == 1
+    //     }
+
+    //     k := i + j
+    //     if k >= len(s3) {
+    //         return true
+    //     }
+    //     if i >= len(s1) {
+    //         memo[i][j] = boolToInt(s2[j:] == s3[k:])
+    //         return memo[i][j] == 1
+    //     }
+    //     if j >= len(s2) {
+    //         memo[i][j] = boolToInt(s1[i:] == s3[k:])
+    //         return memo[i][j] == 1
+    //     }
+
+    //     result := false
+    //     if s1[i] == s3[k] && solve(i+1, j) {
+    //         result = true
+    //     } 
+    //     if s2[j] == s3[k] && solve(i, j+1) {
+    //         result = true
+    //     }
+
+    //     memo[i][j] = boolToInt(result)
+    //     return result
+    // }
+
+    // return solve(0, 0)
+
+    dp := make([][]bool, len(s1) + 1)
+    for i := range dp{
+        dp[i] = make([]bool, len(s2) + 1)
+    }
+
+    for i := range len(s1) + 1{
+        if s1[i:] == s3[i + len(s2):]{
+            dp[i][len(s2)] = true
+        }
+    }
+    for j := range len(s2) + 1{
+        if s2[j:] == s3[j + len(s1):]{
+            dp[len(s1)][j] = true
         }
     }
 
-    var solve func(i, j int) bool
-    solve = func(i, j int) bool {
-        if memo[i][j] != -1 {
-            return memo[i][j] == 1
-        }
 
-        k := i + j
-        if k >= len(s3) {
-            return true
+    for i := len(s1) - 1; i >= 0; i--{
+        for j := len(s2) - 1; j >= 0; j--{
+            if s1[i] == s3[i + j]{
+                dp[i][j] = dp[i][j] || dp[i + 1][j]
+            }
+            if s2[j] == s3[i + j]{
+                dp[i][j] = dp[i][j] || dp[i][j + 1]
+            }
         }
-        if i >= len(s1) {
-            memo[i][j] = boolToInt(s2[j:] == s3[k:])
-            return memo[i][j] == 1
-        }
-        if j >= len(s2) {
-            memo[i][j] = boolToInt(s1[i:] == s3[k:])
-            return memo[i][j] == 1
-        }
-
-        result := false
-        if s1[i] == s3[k] && solve(i+1, j) {
-            result = true
-        } 
-        if s2[j] == s3[k] && solve(i, j+1) {
-            result = true
-        }
-
-        memo[i][j] = boolToInt(result)
-        return result
     }
 
-    return solve(0, 0)
+    return dp[0][0]
 }
 
