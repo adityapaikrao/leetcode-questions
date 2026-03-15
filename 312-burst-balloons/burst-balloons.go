2f93fa224942dafcf1f974318d0fe9aca1466a96
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-15 17:17:30 -0400

    Time: 92 ms (40.00%) | Memory: 7.9 MB (37.65%) - LeetSync

--- 312-burst-balloons/burst-balloons.go
+++ 312-burst-balloons/burst-balloons.go
@@ -39,4 +39,28 @@ func maxCoins(nums []int) int {
 
     return solve(1, len(nums))
 
+    dp := make([][]int, len(coins))
+    for i := range dp{
+        dp[i] = make([]int, len(coins))
+        for j := range dp[i]{
+            if i > j {
+                dp[i][j] = 0
+            } 
+            if i >= 1 && i < len(coins) {
+                dp[i][i] = coins[i] * coins[i-1] * coins[i + 1]
+            }
+        }
+    }
+
+    for l := 1; l <= len(nums); l++{
+        for i := 1; i <= len(nums) - l + 1; i++{
+            j := i + l - 1
+            for k := i; k <= j; k++{
+                curr := coins[i-1] * coins[k] * coins[j + 1]
+                dp[i][j] = max(dp[i][j], curr + dp[i][k-1] + dp[k+1][j])
+            }
+        }
+    }
+
+    return dp[1][len(nums)]
 }
