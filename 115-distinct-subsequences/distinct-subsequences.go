807371401538d5008a2fb0c89e0b2ec2872122d5
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-15 13:59:19 -0400

    Time: 15 ms (49.63%) | Memory: 21.1 MB (20.00%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.go
+++ 115-distinct-subsequences/distinct-subsequences.go
@@ -1,39 +1,30 @@
 func numDistinct(s string, t string) int {
-    
-    memo := make([][]int, len(s) + 1)
-    for i := range memo{
-        memo[i] = make([]int, len(t) + 1)
-        for j := range memo[i]{
-            memo[i][j] = -1
+    if len(t) > len(s){
+        return 0
+    }
+
+    dp := make([][]int, len(t) + 1)
+    for i := range dp{
+        dp[i] = make([]int, len(s) + 1)
+    }
+
+    for i := range dp{
+        dp[i][len(s)] = 0
+    }
+
+    for j := range dp[0]{
+        dp[len(t)][j] = 1
+    }
+
+    for i := len(t) - 1; i >= 0; i--{
+        for j := len(s) - 1; j >= 0; j--{
+            if t[i] == s[j]{
+                dp[i][j] += dp[i + 1][j + 1]
+            }
+            dp[i][j] += dp[i][j + 1]
         }
     }
-    memo[len(s)][len(t)] = 1
 
-    var solve func(i, j int) int 
-    solve = func(i, j int) int {
-        if memo[i][j] != -1{
-            return memo[i][j]
-        }
-
-        // Base Cases:
-        if j == len(t) {
-            return 1
-        }
-        if i == len(s) {
-            return 0
-        }
-
-        // Recurrence
-        numWays := 0
-        if s[i] == t[j] {
-            numWays += solve(i + 1, j + 1)
-        }
-        numWays += solve(i + 1, j) 
-
-        memo[i][j] = numWays
-        return numWays
-    }
-    solve(0, 0)
-    return solve(0, 0)
+    return dp[0][0]
 
 }
