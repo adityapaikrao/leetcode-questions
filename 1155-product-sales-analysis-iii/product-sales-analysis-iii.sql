a6feb0bd36fd3e4c343fe5ab36427524972eb48c
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-17 12:29:18 -0400

    Time: 969 ms (31.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT a.product_id, b.first_year, a.quantity, a.price
+FROM Sales a
+JOIN (
+    SELECT product_id,
+        MIN(year) as first_year
+    FROM Sales
+    GROUP BY product_id
+) b on a.product_id = b.product_id
+AND a.year = b.first_year
+-- GROUP BY a.product_id, b.first_year
