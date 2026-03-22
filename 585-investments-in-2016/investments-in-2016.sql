74860ef6d0b62af97ba81a4887fb97f68d7797ad
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-22 12:57:55 -0400

    Time: 266 ms (49.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,18 @@
+-- Write your PostgreSQL query statement below
+WITH tiv_2015_counts AS (
+    SELECT DISTINCT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(pid) > 1
+),
+lat_lon_counts AS (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(pid) = 1
+)
+
+SELECT ROUND(sum(tiv_2016)::NUMERIC, 2) AS tiv_2016
+FROM Insurance 
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM tiv_2015_counts)
+AND (lat, lon) IN (SELECT lat, lon FROM lat_lon_counts)
