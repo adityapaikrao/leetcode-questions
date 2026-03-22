a493b9df39354aa190c19441fd107dd5069bad56
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-22 13:13:15 -0400

    Time: 244 ms (72.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT b.name AS Department, a.name as Employee, a.salary AS Salary
+FROM
+(
+SELECT name, salary, departmentId, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+FROM Employee
+) a
+JOIN Department b
+ON a.departmentId = b.id
+AND a.rnk <= 3
