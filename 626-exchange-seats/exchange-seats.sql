1cc1c862c0086be0d6877ce8f3e53631c001379a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-21 22:27:15 -0400

    Time: 205 ms (23.75%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,20 +1,8 @@
 -- Write your PostgreSQL query statement below
-WITH adj_ids AS (SELECT 
-    id,
-    LAG(id, 1) OVER(ORDER BY id) as prev_id,
-    LEAD(id, 1) OVER(ORDER BY id) as next_id
+SELECT CASE 
+        WHEN id % 2 = 1 AND (SELECT MAX(id) FROM Seat) = id THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        END AS id, student
 FROM Seat
-)
-
-(SELECT b.id, a.student
-FROM Seat a
-JOIN adj_ids b
-ON a.id = (CASE WHEN b.id % 2 = 1 THEN b.next_id ELSE b.prev_id END)
-ORDER BY b.id
-)
-UNION ALL 
-(
-    SELECT id, student
-    FROM Seat
-    WHERE id = (SELECT MAX(id) FROM Seat HAVING COUNT(*) % 2 = 1)
-)
+ORDER BY id
