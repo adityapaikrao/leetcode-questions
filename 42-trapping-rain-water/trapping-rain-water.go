4e2ed3b4500d7c9c2c64a0976fa7492be2c5964c
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-26 15:38:49 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.60%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -0,0 +1,50 @@
+/*
+
+[0,1,0,2,1,0,1,3,2,1,2,1]
+   0 
+   i 
+                     j
+ lmax = 0
+ rmax = 1
+ water = min(lmax, rmax) - height => 
+
+ if lmax < rmax: 
+    can def store (lmax - height) water at i
+    i++
+    lmax: update
+else:
+    can def store (lmax - height) water at i
+    j++
+    rmax: update
+
+*/
+
+func trap(height []int) int {
+    if len(height) <= 1 {
+        return 0
+    }
+    lmax := height[0]
+    rmax := height[len(height) - 1]
+
+    i := 1
+    j := len(height) - 2
+    water := 0
+
+    for i <= j {
+        // fmt.Printf("i: %v j: %v lmax: %v rmax: %v\n", i, j, lmax, rmax)
+        if lmax <= rmax{
+            if lmax > height[i]{
+                water += lmax - height[i]
+            }
+            lmax = max(lmax, height[i])
+            i++
+        } else{
+            if rmax > height[j]{
+                water += rmax - height[j]
+            }
+            rmax = max(rmax, height[j])
+            j--
+        }
+    }
+    return water
+}
