98ea83600476752a912d20dcaacb2cf7dcd37219
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-03-27 18:44:09 -0400

    Time: 96 ms (5.05%) | Memory: 14.5 MB (14.98%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
@@ -5,22 +5,22 @@ func largestRectangleArea(heights []int) int {
     for i := range heights {
         // previous runs can extend
         leftIdx := i
-        for len(stack) > 0 && heights[stack[len(stack) - 1][1]] > heights[i] {
+        for len(stack) > 0 && stack[len(stack) - 1][1] > heights[i] {
             prev := stack[len(stack) - 1]
             stack = stack[:len(stack) - 1]
             leftIdx = prev[0]
 
-            area := (i - prev[0]) * heights[prev[1]]
+            area := (i - prev[0]) * prev[1]
             if area > maxArea {
                 maxArea = area
             }
         }
-        stack = append(stack, []int{leftIdx, i})
+        stack = append(stack, []int{leftIdx, heights[i]})
     }
     
 
     for i := range stack {
-        area := (len(heights) - stack[i][0]) * heights[stack[i][1]]
+        area := (len(heights) - stack[i][0]) * stack[i][1]
         if area > maxArea {
             maxArea = area
         }
