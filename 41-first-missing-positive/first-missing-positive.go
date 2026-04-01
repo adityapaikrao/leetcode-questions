c69b35c81fae95bf31151183c0793571cec47b98
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-04-01 13:23:59 -0400

    Time: 0 ms (100.00%) | Memory: 10.2 MB (49.24%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.go
+++ 41-first-missing-positive/first-missing-positive.go
@@ -1,40 +1,44 @@
 /*
--1 1 3 4
- m
-i
+[0, 1, 2] -> ideally should be 1 2 3 -> 4 [1, n]
+- mark presence of number using indices [0, n-1]
+- if i is present mark it using index i - 1: set to 1?
+        0, 1, 2 -> 1, 1, 2 : works
+        3, 4, 2, 1 ->  1, 4, 1, 1 : doesnt work (2 instead of 5)
+        - overwriting future values: set to -ve instead & check absolute value?
+        3, 4, 2, 1 -> -3, -4, -2, -1: works!
+        0, 1, 2 -> -0, 1, 2 : zeros will cause issues
+            - in one pass check if 1 present & convert nums not in [1, n] to 1 
 
 */
-import (
-    "slices"
-)
-
 
 func firstMissingPositive(nums []int) int {
-    slices.Sort(nums)
+    onePresent := false
+    n := len(nums)
 
-    i := 0
-    j := len(nums) - 1
-
-    for i <= j{
-        mid := i + (j - i) / 2
-        if nums[mid] <= 0{
-            i = mid + 1
-        } else {
-            j = mid - 1
+    for i := range nums {
+        if nums[i] == 1{
+            onePresent = true
+        } else if nums[i] < 1 || nums[i] > n{
+            nums[i] = 1
         }
     }
 
-    curr := 1
-    for idx := i; idx < len(nums); idx++{
-        if idx > 0 && nums[idx] == nums[idx - 1]{
-            continue
-        }
-        if nums[idx] != curr{
-            return curr
-        }
-        curr++
+    if !onePresent{
+        return 1
     }
 
-    return curr
+    for i := range nums {
+        absVal := max(nums[i], -nums[i])
+        if nums[absVal - 1] > 0 {
+            nums[absVal - 1] *= -1
+        }
+    }
 
+    for i := range nums {
+        if nums[i] > 0 {
+            return i + 1
+        }
+    }
+
+    return n + 1
 }
