40b69eb11df789afc25d4a3f26c2d6f62da57b93
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-04-25 16:10:41 -0400

    Time: 14 ms (74.90%) | Memory: 21.6 MB (24.45%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,29 +1,16 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        num_candies = 1
-        i = 1
+        """
+        1 2 3 1
+        """
+        num_candies = [1] * len(ratings)
 
-        while i < len(ratings):
-            while i < len(ratings) and ratings[i] == ratings[i-1]:
-                i += 1
-                num_candies += 1
-            
-            peak = 1
-            while i < len(ratings) and ratings[i] > ratings[i-1]:
-                peak += 1
-                num_candies += peak
-                i += 1
-            
-            valley = 0
-            while i < len(ratings) and ratings[i] < ratings[i-1]:
-                valley += 1
-                num_candies += valley
-                i += 1
-
-            if valley + 1 > peak:
-                num_candies += valley + 1 - peak
-            
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                num_candies[i] = num_candies[i-1] + 1
+        # print(num_candies)
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and num_candies[i] <= num_candies[i + 1]:
+                num_candies[i] = num_candies[i + 1] + 1
         
-        return num_candies
-        
-
+        return sum(num_candies)
