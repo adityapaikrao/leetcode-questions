5ba4f00dd5424a95ab32e379a437848f3b9ded42
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-05-07 22:09:44 -0400

    Time: 34 ms (35.47%) | Memory: 14.9 MB (41.13%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.go
+++ 239-sliding-window-maximum/sliding-window-maximum.go
@@ -1,23 +1,37 @@
-func maxSlidingWindow(nums []int, k int) []int {
-    q := make([]int, 0, k)
-    maxWindow := make([]int, 0, len(nums) - k + 1)
+/*
+3 2 4 -3 5 3 6 7
+          
+window = [3, 4]
 
-    for i := range nums {
-        // remove out of window elems
-        if len(q) > 0 && q[0] <= i - k {
-            q = q[1:]
+- remove out of bounds elem at each iteration from front
+- pop all smaller elems than curr from back
+
+- will need deque to handle push from back & pop from front
+*/
+
+func maxSlidingWindow(nums []int, k int) []int {
+    maxWindow := make([]int, 0, len(nums) - k)
+    q := make([]int, 0, k)
+
+    for i := range len(nums) {
+        // remove all out of bound elems from front
+        for len(q) > 0 && q[0] <= i - k {
+            q = q[1:] // pop from front
         }
-        // remove smaller elems from q back
+
+        // pop all smaller elems from back 
         for len(q) > 0 && nums[q[len(q) - 1]] <= nums[i] {
             q = q[:len(q) - 1]
         }
 
-        q = append(q, i)
+        q = append(q, i) // add curr elem 
 
         if i >= k - 1{
             maxWindow = append(maxWindow, nums[q[0]])
         }
+
     }
 
-    return maxWindow    
+    return maxWindow
+
 }
