fd6586b05a59e91adf03a636883b2ac61868fc8a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-05-07 21:58:25 -0400

    Time: 41 ms (49.87%) | Memory: 20.2 MB (87.46%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,34 +1,67 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        sign = 1
+        """
+        (1 + (4 + 11) - 3)
+                         i
+        
+        s = [0, 1]
+        ans = 13
         num = 0
-        res = 0
-        stack = []
+        op = 1 
 
-        for i in range(len(s)):
-            if s[i] == " ": continue
-            elif s[i] == "(":
-                stack.extend([res, sign])
-                sign = 1
+        if (:
+            - add prev & op to stack
+            - set ans & num to 0
+        if num:
+            - convert num to num * 10 + digit        
+        if +:
+            - add num * op to ans
+            - set num to 0
+            - set op = 1
+        if ):
+            - add num * op to ans
+            - set num to 0
+
+            - stack pop to get prev res: but how to know if it was x + (exp) or x - (exp)?
+                - stack needs to store the operation as well for the prev
+            
+            - prev, sign = stack.pop() twice
+            - ans = prev + sign * sign
+        """
+
+        stack = []
+        num = 0
+        ans = 0
+        op = 1
+
+        for char in s:
+            if char == " ": continue
+            if char == ")":
+                ans += num * op
                 num = 0
-                res = 0
-            elif s[i] == ")":
-                res += sign * num
-                num = 0
-                sign = 1
 
                 sign = stack.pop()
                 prev = stack.pop()
-                res = res * sign + prev
-            elif s[i] == "+":
-                res += sign * num
-                sign = 1
+                ans = prev + sign * ans
+
+            elif char == "(":
+                stack.extend([ans, op])
                 num = 0
-            elif s[i] == "-":
-                res += sign * num
-                sign = -1
+                ans = 0
+                op = 1
+            
+            elif char == "+":
+                ans += op * num
                 num = 0
+                op = 1
+            
+            elif char == "-":
+                ans += op * num
+                num = 0
+                op = -1
             else:
-                num = num * 10 + int(s[i])
+                num = num * 10 + int(char)
         
-        return res + sign * num
+        return ans + num * op
+
+        
