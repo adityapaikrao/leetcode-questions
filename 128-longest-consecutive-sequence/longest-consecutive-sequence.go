fa8f17a0ac549a368c4f878cb85dacc68cd69571
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-05-24 15:08:50 -0400

    Time: 70 ms (13.16%) | Memory: 12.7 MB (24.15%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -0,0 +1,21 @@
+func longestConsecutive(nums []int) int {
+    set := make(map[int]bool, 0)
+    for _, num := range nums {
+        set[num] = true
+    }
+    maxLen := 0
+
+    for num := range set {
+        if !set[num - 1] && set[num]{
+            currLen := 1 
+            set[num] = false // dont process same number again
+            for set[num + 1] {
+                currLen++
+                num++
+            }
+            maxLen = max(maxLen, currLen)
+        } 
+    }
+
+    return maxLen
+}
