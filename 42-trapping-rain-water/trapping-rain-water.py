1b0ba2a01dc6fbd95519824fd160cf37d1a359c1
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-05-26 15:05:58 -0400

    Time: 3 ms (95.13%) | Memory: 21.1 MB (49.78%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,24 +1,31 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left_max = [height[0]]
-        lmax = height[0]
+        """
+        lmax 0
+        rmax 1
 
-        for i in range(1, len(height)):
-            left_max.append(lmax)
-            if height[i] > lmax: lmax = height[i]
-        
-        right_max = [height[-1]]
+        0,1,0,2,1,0,1,3,2,1,2,1
+          i
+                            j
+
+        if lmax <= rmax: water += lmax - height[i]; i++; lmax = max(lmax, hieght[i])
+
+        """
+        lmax = height[0]
         rmax = height[-1]
 
-        for i in range(len(height) - 2, -1, -1):
-            right_max.append(rmax)
-            if height[i] > rmax: rmax = height[i]
-        
-        right_max.reverse()
-
+        i = 1
+        j = len(height) - 2
         water = 0
 
-        for i in range(1, len(height) - 1):
-            water += max(0, min(left_max[i], right_max[i]) - height[i])
+        while i <= j:
+            if lmax <= rmax:
+                if lmax >= height[i]: water += lmax - height[i]  
+                else: lmax = height[i]
+                i += 1
+            else:
+                if rmax >= height[j]: water += rmax - height[j]  
+                else: rmax = height[j]
+                j -= 1
         
         return water
