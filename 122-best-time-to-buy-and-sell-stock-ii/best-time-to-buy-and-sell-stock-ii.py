76ade715e748d6e7b5dc83abeccee92a7d7d9dae
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-14 17:53:38 -0400

    Time: 19 ms (7.62%) | Memory: 22.3 MB (7.29%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -5,27 +5,34 @@ class Solution:
         b
         s
         """
-        memo = {}
-        def get_profit(index: int, can_buy: bool) -> int:
-            # check memo
-            if (index, can_buy) in memo:
-                return memo[(index, can_buy)]
+        # memo = {}
+        # def get_profit(index: int, can_buy: bool) -> int:
+        #     # check memo
+        #     if (index, can_buy) in memo:
+        #         return memo[(index, can_buy)]
 
-            # Base Case
-            if index >= len(prices):
-                return 0
+        #     # Base Case
+        #     if index >= len(prices):
+        #         return 0
             
-            # Recurse
-            if can_buy:
-                profit = max(-prices[index] + get_profit(index + 1, False), 
-                    get_profit(index + 1, True))
-            else:
-                profit = max(prices[index] + get_profit(index + 1, True), 
-                get_profit(index + 1, False))
+        #     # Recurse
+        #     if can_buy:
+        #         profit = max(-prices[index] + get_profit(index + 1, False), 
+        #             get_profit(index + 1, True))
+        #     else:
+        #         profit = max(prices[index] + get_profit(index + 1, True), 
+        #         get_profit(index + 1, False))
             
-            memo[(index, can_buy)] = profit
-            return profit
+        #     memo[(index, can_buy)] = profit
+        #     return profit
         
-        return get_profit(0, True)
-            
+        # return get_profit(0, True)
+        
+        dp = [[0] * 2 for _ in range(len(prices) + 1)]
+
+        for i in range(len(prices) - 1, -1, -1):
+            dp[i][0] = max(dp[i + 1][1] -prices[i], dp[i + 1][0])
+            dp[i][1] = max(dp[i + 1][0] + prices[i], dp[i + 1][1])
+        
+        return dp[0][0]
 
