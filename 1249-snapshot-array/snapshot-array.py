8175eb12907920366456e439cfcdbc513f7711f9
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-15 11:12:21 -0400

    Time: 184 ms (50.48%) | Memory: 45.9 MB (82.67%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -1,37 +1,31 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.index = {}
-        self.snap_id = 0
+        self._size = length
+        self._indexes = [[] for _ in range(length)]
+        self._snap_id = 0
+
     def set(self, index: int, val: int) -> None:
-        if index not in self.index:
-            self.index[index] = {}
-        self.index[index][self.snap_id] = val
+        self._indexes[index].append((self._snap_id, val))
         return
 
     def snap(self) -> int:
-        self.snap_id += 1
-        return self.snap_id - 1
+        self._snap_id += 1
+        return self._snap_id - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        if index not in self.index:
-            return 0
-        return self._most_recent(index, sorted(list(self.index[index].keys())), snap_id)
-    
-    def _most_recent(self, index: int, snap_ids: List[int], snap_id: int) -> int:
-        most_recent = 0
-        l = 0
-        r = len(snap_ids) - 1
+        val = 0
+        l, r = 0, len(self._indexes[index]) - 1
+
         while l <= r:
             mid = (l + r) // 2
-            if snap_ids[mid] <= snap_id:
-                most_recent = self.index[index][snap_ids[mid]]
-                l = mid + 1
-            else:
+            if self._indexes[index][mid][0] > snap_id:
                 r = mid - 1
-        return most_recent
-            
-
+            else:
+                val = self._indexes[index][mid][1]
+                l = mid + 1
+        
+        return val
 
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
