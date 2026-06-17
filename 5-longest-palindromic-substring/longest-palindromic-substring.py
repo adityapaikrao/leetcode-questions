446c452c6d727f562ecb4b6d4012e7cae0e6737b
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-17 17:17:44 -0400

    Time: 243 ms (77.44%) | Memory: 19.1 MB (91.85%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,28 +1,38 @@
+from typing import Tuple
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        longest = 1
-        start = 0
+        def longest_odd_palindrome(start_index: int) -> Tuple[int, int]:
+            i, j = start_index - 1, start_index + 1
+            if i < 0 or j > len(s):
+                return start_index, start_index
+            
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            
+            return i + 1, j - 1
         
-        dp = [[False] * n for _ in range(n)]
+        def longest_even_palindrome(start_index: int) -> Tuple[int, int]:
+            i, j = start_index - 1, start_index
+            if i < 0:
+                return -1, -1
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            
+            return i + 1, j - 1
+        
+        longest_start = 0
+        longest_end = 0
 
-        for i in range(n):
-            dp[i][i] = True
+        for i in range(len(s)):
+            odd_start, odd_end = longest_odd_palindrome(i)
+            if odd_end - odd_start > longest_end - longest_start:
+                longest_start, longest_end = odd_start, odd_end
         
-        for i in range(1, n):
-            if s[i-1] == s[i]:
-                dp[i-1][i] = True
-                longest = 2
-                start = i - 1
+            even_start, even_end = longest_even_palindrome(i)
+            if even_end - even_start > longest_end - longest_start:
+                longest_start, longest_end = even_start, even_end
         
-        for k in range(3, n + 1):
-            for i in range(n - k + 1):
-                j = i + k - 1
-                if s[i] == s[j] and dp[i+1][j-1]:
-                    dp[i][j] = True
-                    if k > longest:
-                        longest = k
-                        start = i
-        return s[start: start + longest]
-
-        
+        return s[longest_start: longest_end + 1]
