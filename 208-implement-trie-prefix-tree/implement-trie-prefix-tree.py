31c99b1ea5d12f1a95aa276ea7bfcb8e46b55c47
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-18 11:11:22 -0400

    Time: 78 ms (6.10%) | Memory: 37.2 MB (7.49%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -2,6 +2,22 @@ class Node:
     def __init__(self):
         self.links = [None] * 26
         self.is_end = False
+    
+    def isEnd(self) -> bool:
+        return self.is_end
+    
+    def setEnd(self):
+        self.is_end = True
+    
+    def hasChar(self, char: str) -> bool:
+        return (self.links[ord(char) - ord('a')] is not None)
+    
+    def addChar(self, char: str):
+        self.links[ord(char) - ord('a')] = Node()
+    
+    def getCharNode(self, char: str) -> Optional[Node]:
+        if not self.hasChar(char): return None
+        return self.links[ord(char) - ord('a')]
 
 class Trie:
 
@@ -11,24 +27,24 @@ class Trie:
     def insert(self, word: str) -> None:
         node = self._root
         for char in word:
-            if not node.links[ord(char) - ord('a')]:
-                node.links[ord(char) - ord('a')] = Node()
-            node = node.links[ord(char) - ord('a')]
-        
-        node.is_end = True
+            if not node.hasChar(char):
+                node.addChar(char)
+            node = node.getCharNode(char)
+
+        node.setEnd()
 
     def search(self, word: str) -> bool:
         node = self._root
         for char in word:
-            if not node.links[ord(char) - ord('a')]: return False
-            node = node.links[ord(char) - ord('a')]
-        return node.is_end
+            if not node.hasChar(char): return False
+            node = node.getCharNode(char)
+        return node.isEnd()
 
     def startsWith(self, prefix: str) -> bool:
         node = self._root
         for char in prefix:
-            if not node.links[ord(char) - ord('a')]: return False
-            node = node.links[ord(char) - ord('a')]
+            if not node.hasChar(char): return False
+            node = node.getCharNode(char)
         return True
 
 
