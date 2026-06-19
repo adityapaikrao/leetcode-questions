fd16d8f4a72bb51f75bd6d89814dd483dfbe3316
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 14:58:29 -0400

    Time: 19 ms (53.99%) | Memory: 20.3 MB (38.76%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,27 +1,12 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        memo = {}
-        def path_sum(row_idx: int, col_idx: int) -> int:
-            if (row_idx, col_idx) in memo:
-                return memo[(row_idx, col_idx)]
+        for i in range(n - 2, -1, -1):
+            for j in range(n):
+                matrix[i][j] += min(
+                    matrix[i + 1][j-1] if j >= 1 else float('inf'),
+                    matrix[i + 1][j],
+                    matrix[i + 1][j+1] if j < n - 1 else float('inf')
+                )
 
-            if col_idx >= n or col_idx < 0: 
-                return float('inf')
-            if row_idx == n:
-                return 0
-            
-            
-            curr_sum = matrix[row_idx][col_idx] 
-            next_max = min(
-                path_sum(row_idx + 1, col_idx - 1),
-                path_sum(row_idx + 1, col_idx + 1),
-                path_sum(row_idx + 1, col_idx)
-            )
-            memo[(row_idx, col_idx)] = curr_sum + next_max
-            return memo[(row_idx, col_idx)]
-        
-        return min(path_sum(0, j) for j in range(n))
-
-
-            
+        return min(matrix[0][j] for j in range(n))
