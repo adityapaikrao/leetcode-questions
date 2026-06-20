d3d00bd3d40c2b188e8656c9e246a6ed5f4b4d29
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 22:35:08 -0400

    Time: 272 ms (16.72%) | Memory: 21.6 MB (48.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -9,12 +9,14 @@ class Solution:
                 new_row = row + row_off
                 new_col = col + col_off
                 if 0 <= new_row < n and 0 <= new_col < m and grid[new_row][new_col] == "1":
+                    grid[row][col] = "0" # mark as visited
                     dfs(new_row, new_col)
         
         num_islands = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
+                    grid[i][j] = "0" # mark as visited
                     dfs(i, j)
                     num_islands += 1
         return num_islands
