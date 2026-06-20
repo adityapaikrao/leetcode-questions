4f87aa49d3a8d06048fe442473e69a77784fa984
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 22:55:12 -0400

    Time: 76 ms (21.38%) | Memory: 20.9 MB (87.52%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,34 +1,27 @@
 class Solution:
-    """
-     3 + 2 * 2
-             i
-
-    exp = 3 
-    prev = 2
-    op = *
-
-    - skip spaces
-    - read number 
-    - check op:
-        + => add to exp; set prev num to num
-        - => subtract from exp; set prev num to -num
-        / => exp - prev num; exp + prevum / num; prevnum = num
-        * => exp - prev num; exp + prevnum * num; prevnum = num
-    - skip spaces
-    - read operation
-    """
     def calculate(self, s: str) -> int:
+        """
+        exp = 0
+        prev = 0
+        op = +
+
+        3 + 5 / 2
+            i
+
+        num = 3
+        """
+        i = 0 
         prev = 0
         op = "+"
         exp = 0
-        i = 0
 
         while i < len(s):
             # skip spaces
             while i < len(s) and s[i] == " ":
                 i += 1
-
-            # read number
+            
+            if i == len(s): break
+            # read num
             num = 0
             while i < len(s) and s[i].isdigit():
                 num = num * 10 + int(s[i])
@@ -49,18 +42,14 @@ class Solution:
                 exp -= prev
                 exp += int(prev / num)
                 prev = int(prev / num)
-            
+
             # skip spaces
             while i < len(s) and s[i] == " ":
                 i += 1
-
-            # read next op    
-            if i < len(s):
-                op = s[i]
-                i += 1
+            
+            # read next operation
+            op = s[i] if i < len(s) else ""
+            i += 1
         
         return exp
-        
             
-            
-
