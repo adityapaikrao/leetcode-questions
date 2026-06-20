8625a0d9ca0f1d8571b92c4f62fc6e2cec4ea160
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 22:10:01 -0400

    Time: 0 ms (100.00%) | Memory: 22.4 MB (85.93%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,25 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         """
-        [[1, 6], [8, 10], [15, 18], [2, 6]]
-                            p
+        [[1,6],[8,10],[15,18],[15,18]]
+                        p
                                       i
+        if overlap:
+            p[1] = max(endtimes)
+        else:
+            p++
+            p = i
         
-        [1, 3] [4, 5]
-                p
-                i
-
+            
         """
         if len(intervals) == 1:
             return intervals
-
+        
         intervals.sort(key = lambda x : x[0])
         prev = 0
         for i in range(1, len(intervals)):
-            if intervals[prev][1] >= intervals[i][0]:
-                intervals[prev][1] = max(intervals[prev][1], intervals[i][1]) 
+            if intervals[i][0] <= intervals[prev][1]:
+                intervals[prev][1] = max(intervals[prev][1], intervals[i][1])
             else:
                 prev += 1
-                if prev != i: intervals[prev] = intervals[i]
-
-        return intervals[:prev + 1] 
+                intervals[prev] = intervals[i]
+        return intervals[:prev + 1]
+        
