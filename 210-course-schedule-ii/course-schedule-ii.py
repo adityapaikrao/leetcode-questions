00c033b0b4ad4387c3f76f0b90b4fa2d7b14060f
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 22:24:53 -0400

    Time: 7 ms (28.07%) | Memory: 20.4 MB (72.76%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,36 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        """
-        0 -> 1
-
-        0 -> 1 -> 3
-          -> 2 -> 
-
-        topological ordering: Kahn's
-        """
-        adj = [[] for _ in range(numCourses)]
-        in_degree = [0] * numCourses
-        for course, prereq in prerequisites:
-            adj[prereq].append(course)
-            in_degree[course] += 1
+        n = numCourses
+        adj = [[] for _ in range(n)]
+        in_degree = [0] * n
+        for curr, prev in prerequisites:
+            adj[prev].append(curr)
+            in_degree[curr] += 1
         
-        q = deque()
-        for course in range(numCourses):
-            if in_degree[course] == 0:
-                in_degree[course] -= 1
+        q = deque([])
+        for course in range(n):
+            if in_degree[course] == 0: 
                 q.append(course)
+                in_degree[course] -= 1 
         
-        if not q: return []
-        # print(q, in_degree)
+        if not q: 
+            return []
+        
         order = []
         while q:
-            curr_course = q.popleft()
-            order.append(curr_course)
-
-            for next_course in adj[curr_course]:
-                in_degree[next_course] -= 1
-                if in_degree[next_course] == 0:
-                    in_degree[next_course] -= 1
-                    q.append(next_course)
+            node = q.popleft()
+            order.append(node)
+            for nbr in adj[node]:
+                in_degree[nbr] -= 1
+                if in_degree[nbr] == 0:
+                    q.append(nbr)
+                    in_degree[nbr] -= 1
         
-        return order if len(order) == numCourses else []
+        return order if len(order) == n else []
+        
