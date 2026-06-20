7adba4e7fae16db123525a84cfae3d8f526e9322
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-19 23:00:19 -0400

    Time: 15 ms (43.48%) | Memory: 24 MB (9.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-        eat tea tan ate nat bat
-
-        bat  nat, tan   ate, tea, eat
-
-        anagram -> same chars with same frequency 
-        eat: e1, a1, t1 -> [1, 0, 0, 0, 1,.....1,, ] 26-length char & store it in map
-        tea: t1 e1 a1
-        anagrams = []
-        """
-
-        hmap = defaultdict(list)
+        anagrams = defaultdict(list)
         for word in strs:
-            freq = [0] * 26
+            char_counts = [0] * 26
             for char in word:
-                freq[ord(char) - ord('a')] += 1
-            hmap[tuple(freq)].append(word)
+                char_counts[ord(char) - ord("a")] += 1
+            anagrams[tuple(char_counts)].append(word)
         
-        return list(hmap.values())
+        return [words for words in anagrams.values()]
