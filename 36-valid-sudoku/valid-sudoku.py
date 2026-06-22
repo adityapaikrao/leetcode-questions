cbcaf5795138829b278762085440ca26c31087f3
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-06-22 17:54:20 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.52%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_set = [set() for _ in range(9)]
+        col_set = [set() for _ in range(9)]
+        grid_set = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".": continue
+                if board[i][j] in row_set[i] or board[i][j] in col_set[j] or board[i][j] in grid_set[i // 3][j // 3]:
+                    return False
+                
+                row_set[i].add(board[i][j])
+                col_set[j].add(board[i][j])
+                grid_set[i // 3][j // 3].add(board[i][j])
+        
+        return True
