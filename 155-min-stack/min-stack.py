7fa390d14544dc107c7e4c8a301eab057c3bce4d
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-04 23:05:09 -0400

    Time: 108 ms (24.15%) | Memory: 31.6 MB (33.34%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,56 +1,66 @@
 """
-2 3 f 1
-flag = new_min - prev_min (1 - 2 = -1)
-min = 1
-
-when popping elem, if elem is min :
-    - min = popped - flag (1 - -1 = 2)
-
-1 2 3 
-
+[2, 4, 2, 1]
 
 min = 1
+
+when min elem is popped -- need to recover prev min
+need some flag to tell when new_min popped
+
+on new min push
+    new_min < prev_min
+    2*new_min < prev_min + new_min
+    new_min > 2*new_min - prev_min
+
+    flag = 2 * new_miin - prev_min
+
+when flag popped ie. a value less than curr_min
+    => new_min popped
+    recover prev_min = 2 * new_min - flag
 
 """
+
 
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.min = None
-
-    def push(self, val: int) -> None:
-        if self.min is None:
-            self.stack.append(val)
-            self.min = val
+        self.stack: List[int] = []
+        self.min_val: int = None
         
-        elif self.min > val:
-            self.stack.append(2*val - self.min)
-            self.min = val
+    def push(self, value: int) -> None:
+        if not self.stack:
+            self.stack.append(value)
+            self.min_val = value
+            return
+        
+        if value >= self.min_val:
+            self.stack.append(value)
         else:
-            self.stack.append(val)
+            flag = 2 * value - self.min_val
+            self.stack.append(flag)
+            self.min_val = value
+        
 
     def pop(self) -> None:
+        if not self.stack:
+            return 
+        
         popped = self.stack.pop()
-
-        if popped < self.min:
-            self.min = 2 * self.min - popped
-        elif not self.stack:
-            self.min = None
+        if popped < self.min_val:
+            # min val popped, recover prev min
+            self.min_val = 2 * self.min_val - popped
+        return
 
     def top(self) -> int:
-        if self.stack[-1] < self.min:
-            return self.min
-        else:
-            return self.stack[-1]
+        return self.stack[-1] if self.stack[-1] > self.min_val else self.min_val
 
     def getMin(self) -> int:
-        return self.min
+        return self.min_val
+        
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(val)
+# obj.push(value)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
