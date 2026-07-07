cbf9f7a712cd87462587466720562ec5962a2403
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-07 00:28:00 -0400

    Time: 7 ms (22.84%) | Memory: 19.3 MB (51.46%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,22 +1,36 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         word_set = set(wordDict)
-        memo = {}
+        # memo = {}
+        # def can_break(start: int) -> bool:
+        #     if start in memo:
+        #         return memo[start]
 
-        def can_break(start: int) -> bool:
-            if start in memo:
-                return memo[start]
+        #     if start >= len(s):
+        #         return True
+            
+        #     for i in range(start, len(s)):
+        #         if s[start: i + 1] in word_set:
+        #             if can_break(i + 1):
+        #                 memo[start] = True
+        #                 return True
+            
+        #     memo[start] = False
+        #     return False
+        """
+        l e e t c o d e 
+        i
+              j
+        0 0 0 0 1 0 0 0 1
+                       
+        """
 
-            if start >= len(s):
-                return True
-            
-            for i in range(start, len(s)):
-                if s[start: i + 1] in word_set:
-                    if can_break(i + 1):
-                        memo[start] = True
-                        return True
-            
-            memo[start] = False
-            return False
-        
-        return can_break(0)
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i: j + 1] in word_set:
+                    dp[i] |= dp[j + 1]
+                    # break
+        return dp[0]
