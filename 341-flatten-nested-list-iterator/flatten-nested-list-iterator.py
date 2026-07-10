f8e1e179fc23639caf92a854672f1060659bebae
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-10 12:05:08 -0400

    Time: 65 ms (31.96%) | Memory: 21.7 MB (84.38%) - LeetSync

--- 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
@@ -19,26 +19,29 @@
 #        @return the nested list that this NestedInteger holds, if it holds a nested list
 #        Return None if this NestedInteger holds a single integer
 #        """
+"""
+[1, [2, 3], [[4]], [[]]]
+     i
+ 1
+recurse on [2, 3] 
+"""
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.numbers = deque()
-
-        def flatten(nested: [NestedInteger]) -> None:
-            for nested_int in nested:
+        self.flattened = deque()
+        def flatten(nested_list: [NestedInteger]):
+            for nested_int in nested_list:
                 if nested_int.isInteger():
-                    self.numbers.append(nested_int.getInteger())
+                    self.flattened.append(nested_int.getInteger())
                 else:
                     flatten(nested_int.getList())
-            return
         flatten(nestedList)
 
     def next(self) -> int:
-        return self.numbers.popleft()
+        return self.flattened.popleft()
     
     def hasNext(self) -> bool:
-        return len(self.numbers) > 0
-
+         return len(self.flattened) > 0
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
