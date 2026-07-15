b64d62df8c5fe45b7e09fefb232f1090a3dc532f
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-15 19:27:18 -0400

    Time: 239 ms (67.62%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,17 +1,6 @@
 -- Write your PostgreSQL query statement below
--- WITH user_confirmations AS (
-SELECT 
-    sg.user_id, 
-    ROUND(AVG(CASE WHEN cnf.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
-    FROM Signups sg
-    LEFT JOIN Confirmations cnf
-    ON sg.user_id = cnf.user_id
-    GROUP BY sg.user_id
--- )
-
--- SELECT 
---     user_id, 
---     ROUND((CASE WHEN user_timeouts = 0 AND user_confirmations = 0 THEN 0 ELSE user_confirmations/(user_timeouts + user_confirmations) END), 2) AS confirmation_rate
-
---     FROM user_confirmations
-    
+SELECT s.user_id, ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 2) AS confirmation_rate
+FROM Signups s 
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
