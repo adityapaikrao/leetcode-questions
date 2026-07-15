b8fdddcd59906db33fa41d8489094e0ec03fa5f5
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-15 18:44:11 -0400

    Time: 285 ms (92.95%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,11 @@
 -- Write your PostgreSQL query statement below
-SELECT name
-FROM Employee 
-WHERE id IN (
-    SELECT managerId
-    FROM Employee 
-    GROUP BY managerId
-    HAVING count(id) >= 5
-)
+SELECT e2.name
+FROM
+(
+    SELECT e1.managerId
+    FROM Employee e1
+    GROUP BY e1.managerId
+    HAVING count(e1.id) >= 5
+) a 
+JOIN Employee e2
+ON a.managerId = e2.id
