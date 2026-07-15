7e2c293a9048332f2be277ce9bff9dbcb7e12139
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-15 13:38:16 -0400

    Time: 356 ms (56.35%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT DISTINCT author_id AS id
-FROM Views
+FROM Views 
 WHERE viewer_id = author_id
 ORDER BY id ASC
