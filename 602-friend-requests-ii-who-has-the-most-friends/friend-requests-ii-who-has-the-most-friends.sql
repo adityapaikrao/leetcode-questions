3a5eafa85bb122cb43eb6d1e682be61b027055e7
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-16 10:55:35 -0400

    Time: 209 ms (18.16%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,24 +1,18 @@
 -- Write your PostgreSQL query statement below
-WITH most_requests AS (
-    SELECT requester_id, COUNT(*) AS count_friends
+WITH friend_counts AS (
+    SELECT requester_id AS id, COUNT(*) AS counts
     FROM RequestAccepted
     GROUP BY requester_id
-),
-most_accepts AS (
-    SELECT accepter_id, COUNT(*) AS count_friends
+
+    UNION ALL
+
+    SELECT accepter_id AS id, COUNT(*) AS counts
     FROM RequestAccepted
     GROUP BY accepter_id
 )
 
-SELECT a.id, SUM(a.num) AS num
-FROM
-(
-SELECT requester_id AS id, count_friends AS num
-FROM most_requests
-UNION ALL
-SELECT accepter_id AS id, count_friends AS num 
-FROM most_accepts
-) a
-GROUP BY a.id
+SELECT id, SUM(counts) as num
+FROM friend_counts
+GROUP BY id
 ORDER BY num DESC
 LIMIT 1
