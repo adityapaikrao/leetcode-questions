502434c2176c0c4e92f54dd2438aa30010348f5a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-16 10:20:56 -0400

    Time: 354 ms (67.32%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,30 +1,26 @@
 -- Write your PostgreSQL query statement below
-WITH products_at_10 AS (
+WITH products_10 AS (
     SELECT product_id
     FROM Products 
     GROUP BY product_id
     HAVING MIN(change_date) > '2019-08-16'
+), products_changed AS (
+    SELECT product_id, change_date
+    FROM Products 
+    WHERE change_date <= '2019-08-16'
 )
+
+SELECT product_id, 10 AS price 
+FROM products_10
+
+UNION
 
 SELECT a.product_id, a.new_price AS price
 FROM Products a
-RIGHT JOIN
-(SELECT product_id, MAX(change_date) AS max_date
-FROM Products
-WHERE change_date <= '2019-08-16'
-GROUP BY product_id
-) b 
+JOIN (
+    SELECT product_id, MAX(change_date) AS change_date_max
+    FROM products_changed
+    GROUP BY product_id
+) b
 ON a.product_id = b.product_id
-AND a.change_date = b.max_date
-UNION
-( SELECT product_id, 10 AS price FROM products_at_10)
-
--- SELECT product_id, MAX(change_date) AS max_date
--- FROM Products
--- WHERE change_date <= '2019-08-16'
--- GROUP BY product_id
-
--- SELECT product_id
---     FROM Products 
---     GROUP BY product_id
---     HAVING MIN(change_date) > '2019-08-16'
+AND a.change_date = b.change_date_max
