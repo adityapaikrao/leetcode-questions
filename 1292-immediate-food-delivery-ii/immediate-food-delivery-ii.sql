bae7f8c0a8ba7cd63f04bb1d4cc56d8c9b6fb7ad
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-15 20:55:46 -0400

    Time: 368 ms (83.24%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,14 +1,6 @@
 -- Write your PostgreSQL query statement below
-WITH customer_first_orders AS (
-    SELECT 
-        order_date, customer_pref_delivery_date,
-        ROW_NUMBER() OVER (
-            PARTITION BY customer_id
-            ORDER BY order_date ASC
-            ) AS row_number
-    FROM Delivery
-)
-
-SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1.0 ELSE 0.0 END), 4) * 100 AS immediate_percentage
-FROM customer_first_orders
-WHERE row_number = 1
+SELECT ROUND(AVG((order_date = customer_pref_delivery_date)::INT), 4) * 100 AS immediate_percentage
+FROM Delivery a
+JOIN (SELECT customer_id, MIN(order_date) as min_order_date FROM Delivery GROUP BY 1) b 
+ON a.customer_id = b.customer_id
+AND a.order_date = b.min_order_date
