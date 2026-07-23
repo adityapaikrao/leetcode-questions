4b33d341714114d52bc50350ee3f6c565ab25a93
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-23 17:58:46 -0400

    Time: 3 ms (97.85%) | Memory: 19.3 MB (60.43%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,17 +1,17 @@
 class Solution:
     def minOperations(self, s: str) -> int:
         """
-        1 1 1 1
-              i
-        p = 0
-        ops = 1
+        0 1 0 0 -> 0 1 0 1 (1) OR 1 0 1 0 (3)
+        0 0 1 0 -> 0 1 0 1 (3) OR 1 0 1 0 (1)
         """
+        ops = 0
         prev = s[0]
-        num_ops = 0
         for i in range(1, len(s)):
             if s[i] == prev:
-                num_ops += 1
-                prev = '1' if s[i] == '0' else '0'
+                # need to change 
+                ops += 1
+                prev = '0' if s[i] == '1' else '1'
             else:
                 prev = s[i]
-        return min(num_ops, len(s) - num_ops)
+            
+        return min(ops, len(s) - ops)
