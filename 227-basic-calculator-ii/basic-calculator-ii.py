6b2630e4a44240f7a7b37e1ae2f9ad66812188bc
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-23 18:11:10 -0400

    Time: 87 ms (14.21%) | Memory: 20.8 MB (92.94%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,55 +1,56 @@
 class Solution:
     def calculate(self, s: str) -> int:
         """
-        exp = 0
-        prev = 0
+        3 + 2 * 2 = 7
+        
+        result = 5
         op = +
+        prev = 2
 
-        3 + 5 / 2
-            i
-
-        num = 3
+        if op == + or -:
+            result += currnum or -currnum
+        else:
+            result -= prev # restore prev num
+            result += prev * currnum
         """
-        i = 0 
         prev = 0
         op = "+"
-        exp = 0
+        result = 0
 
+        i = 0
         while i < len(s):
-            # skip spaces
+            # skip space
             while i < len(s) and s[i] == " ":
                 i += 1
-            
-            if i == len(s): break
-            # read num
-            num = 0
-            while i < len(s) and s[i].isdigit():
-                num = num * 10 + int(s[i])
-                i += 1
-            
-            # do operation
-            if op == "+":
-                exp += num
-                prev = num
-            elif op == "-":
-                exp -= num
-                prev = -num
-            elif op == "*":
-                exp -= prev
-                exp += prev * num
-                prev = prev * num
-            else:
-                exp -= prev
-                exp += int(prev / num)
-                prev = int(prev / num)
+            if i == len(s): 
+                break
 
-            # skip spaces
-            while i < len(s) and s[i] == " ":
+            # read num
+            curr_num = 0
+            while i < len(s) and s[i].isnumeric():
+                curr_num = curr_num * 10 + int(s[i])
                 i += 1
             
-            # read next operation
-            op = s[i] if i < len(s) else ""
-            i += 1
-        
-        return exp
+            # perform op
+            if op in ['+', '-']:
+                result += curr_num if op == '+' else -curr_num
+                prev = curr_num if op == '+' else -curr_num
+            elif op == '/':
+                result -= prev
+                result += int(prev / curr_num)
+                prev = int(prev / curr_num)
+            else:
+                result -= prev
+                result += prev * curr_num
+                prev = prev * curr_num
             
+            # skip spaces
+            while i < len(s) and s[i] == " ":
+                i += 1  
+            
+            # read next op
+            if i < len(s):
+                op = s[i]
+                i += 1
+        
+        return result
