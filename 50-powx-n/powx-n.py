fc6491667e592b646c14b77b50610d9b400e2249
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-24 19:25:22 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (85.69%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,19 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         # Base Cases
+        if x == 0:
+            return 0
         if n == 0:
             return 1
-        if x == -1:
-            return (-1) ** (n % 2)
-        if n == 1 or x == 1:
+        if n == 1:
             return x
         
         if n < 0:
-            x = (1/x)
-            n = -n
-        
-        if n % 2 == 0:
-            return self.myPow(x, n // 2) ** 2
+            x = 1 / x
+            n  = -n
+        exp = self.myPow(x, n // 2)
+        if n % 2 == 1:
+            return x * exp * exp
         else:
-            return x * ((self.myPow(x, n // 2)) ** 2)
+            return exp * exp
     
+
+        
+        
