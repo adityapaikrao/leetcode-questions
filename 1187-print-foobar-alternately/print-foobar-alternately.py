292fc148badf03fe2bb04791e32025e20c6af395
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-24 13:39:54 -0400

    Time: 96 ms (5.99%) | Memory: 19.9 MB (51.20%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.py
+++ 1187-print-foobar-alternately/print-foobar-alternately.py
@@ -1,24 +1,32 @@
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.fooLock = threading.Lock()
-        self.barLock = threading.Lock()
-        self.barLock.acquire()
+        self.foo_ = True
+        self.cv = threading.Condition()
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
 
             # printFoo() outputs "foo". Do not change or remove this line.
-            self.fooLock.acquire()
-            printFoo()
-            self.barLock.release()
+            with self.cv:
+                while not self.foo_:
+                    self.cv.wait()
+    
+                printFoo()
+                self.foo_ = False
+                self.cv.notify_all()
+            
 
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
             # printBar() outputs "bar". Do not change or remove this line.
-            self.barLock.acquire()
-            printBar()
-            self.fooLock.release()
+            with self.cv:
+                while self.foo_:
+                    self.cv.wait()
+                
+                printBar()
+                self.foo_ = True
+                self.cv.notify_all()
