56472afbf622a17dbd5bdff6d0b4b7967f968641
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-25 11:17:50 -0400

    Time: 8 ms (22.38%) | Memory: 19.6 MB (74.37%) - LeetSync

---
+++ 393-utf-8-validation/utf-8-validation.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        i = 0
+        """
+        11000101 10000010 00000001
+          k
+                            i   
+                    
+        1 1 1 1 1 1 1 0
+        nbytes = 0 + 1 + 1 = 2
+        """
+        while i < len(data):
+            # first octet that denotes the n-bytes
+            k = 7
+            curr = data[i]
+            num_bytes = 0
+            while k >= 0 and curr & (1 << k):
+                num_bytes += 1
+                k -= 1
+            if num_bytes == 0:
+                i += 1
+                continue
+            
+            if num_bytes == 1 or num_bytes > 4 or (i + num_bytes) > len(data): 
+                return False
+            
+            for j in range(i + 1, i + num_bytes):
+                if (((data[j] >> 7) & 1) == 0) or (((data[j] >> 6) & 1) == 1):
+                    return False
+            i = i + num_bytes
+        
+        return True
