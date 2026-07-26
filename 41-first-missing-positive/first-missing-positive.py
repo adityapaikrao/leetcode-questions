5812fdf51c12a027b46b98c6f4e4588960308be8
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-25 21:33:21 -0400

    Time: 66 ms (11.18%) | Memory: 31.1 MB (30.88%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,43 +1,44 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         """
-        3 4 -1 1 : -> 2
-        1 2 0: -> 3
+        1 2 0 -> 3
+        3 4 -1 1 -> 2
 
-        ideally should have numbers 1 2 3 ... n for n length array [1, n]
-        use indices to mark
+        ideally should have numbers 1 to n
+        1 2 3 4 -> then missing = n + 1
 
-        -3 -4 -1 1
-        0 1  2 3
+        0 1 2 3 indices every index -> if index + 1 exists in arr
+        1 2 3 4
 
-        mark 3 is present at index 3 - 1: 2 convert num at 2 to negative 
+        one_present = True
+        3 4 -1 1
+        -> 3 4 -1 -1
 
+        1 2 0
         -1 -2 0
-        0 1 2 -> works
-
-        3 2 1 5
-        0 1 2 3 
-
-        how do we know the number was "marked" negative vs actually negative number
-        - first pass convert all zeros & negatives to n + 1
-        - second pass: mark nums[nums[i] - 1] to negative
-        - third pass: check i where nums[i] > 0; then answer is i + 1
-        if no such i found then return n + 1
+        -> 
         
-        i
-        -3 4 -5 -1
-        0 1 2  3
+        2 1 
+        -2 -1
         """
         n = len(nums)
+        one_present = False
         for i in range(n):
+            if nums[i] == 1:
+                one_present = True
             if nums[i] <= 0:
-                nums[i] = n + 1
+                nums[i] = 1
+        
+        if not one_present:
+            return 1
         
         for i in range(n):
-            if abs(nums[i]) <= n and nums[abs(nums[i]) - 1] > 0: 
-                nums[abs(nums[i]) - 1] *= -1
-
-        for i in range(n):
-            if nums[i] > 0: return i + 1
+            if 1 <= abs(nums[i]) <= n:
+                if nums[abs(nums[i]) - 1] > 0:
+                    nums[abs(nums[i]) - 1] *= -1
+        # print(nums)
+        for i in range(1, n):
+            if nums[i] > 0:
+                return i + 1
         
         return n + 1
