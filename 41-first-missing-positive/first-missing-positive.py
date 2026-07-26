bd8a58b6fd1c3c07e039bd5044782c5e823d0a5c
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-25 21:40:08 -0400

    Time: 59 ms (24.65%) | Memory: 30.9 MB (61.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,44 +1,24 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         """
-        1 2 0 -> 3
-        3 4 -1 1 -> 2
-
-        ideally should have numbers 1 to n
-        1 2 3 4 -> then missing = n + 1
-
-        0 1 2 3 indices every index -> if index + 1 exists in arr
-        1 2 3 4
-
-        one_present = True
         3 4 -1 1
-        -> 3 4 -1 -1
 
-        1 2 0
-        -1 -2 0
-        -> 
-        
-        2 1 
-        -2 -1
+        cycle sort 
+        3 4 -1 1
+        1 4 3 -1
         """
+        i = 0
         n = len(nums)
-        one_present = False
+
+        while i < len(nums):
+            correct_idx = nums[i] - 1
+            if 1 <= nums[i] <= n and nums[i] != nums[correct_idx]:
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+            else:
+                i += 1
+
         for i in range(n):
-            if nums[i] == 1:
-                one_present = True
-            if nums[i] <= 0:
-                nums[i] = 1
-        
-        if not one_present:
-            return 1
-        
-        for i in range(n):
-            if 1 <= abs(nums[i]) <= n:
-                if nums[abs(nums[i]) - 1] > 0:
-                    nums[abs(nums[i]) - 1] *= -1
-       
-        for i in range(1, n):
-            if nums[i] > 0:
+            if nums[i] != i + 1:
                 return i + 1
         
         return n + 1
