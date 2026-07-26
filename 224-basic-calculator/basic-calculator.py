1c274d09425b05dcd2681a0f186a34908c78165a
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-26 15:19:12 -0400

    Time: 37 ms (60.36%) | Memory: 20.2 MB (61.45%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,67 +1,58 @@
 class Solution:
     def calculate(self, s: str) -> int:
         """
-        (1 + (4 + 11) - 3)
-                         i
+        1+(4+5+2)-3+(6+8)
+                i 
+
+        eval = 0 + 4 + 5 + 2
         
-        s = [0, 1]
-        ans = 13
-        num = 0
-        op = 1 
 
-        if (:
-            - add prev & op to stack
-            - set ans & num to 0
-        if num:
-            - convert num to num * 10 + digit        
-        if +:
-            - add num * op to ans
-            - set num to 0
-            - set op = 1
+        stack = [1, +]
+
+        if "(":
+            stack + [eval, op]
+            eval = 0        
+            op = +1
+        if +, -:
+            eval += num * op
+            op = -1
+            num = 0
         if ):
-            - add num * op to ans
-            - set num to 0
+            prev, prev_op = stack.pop() twice
+            eval = prev + prev_op * num
+            num = 0
+            op = 1
+        else:
+            num = num * 10 + digit
 
-            - stack pop to get prev res: but how to know if it was x + (exp) or x - (exp)?
-                - stack needs to store the operation as well for the prev
-            
-            - prev, sign = stack.pop() twice
-            - ans = prev + sign * sign
         """
-
         stack = []
         num = 0
-        ans = 0
         op = 1
+        result = 0
 
         for char in s:
             if char == " ": continue
-            if char == ")":
-                ans += num * op
-                num = 0
-
-                sign = stack.pop()
-                prev = stack.pop()
-                ans = prev + sign * ans
-
-            elif char == "(":
-                stack.extend([ans, op])
-                num = 0
-                ans = 0
-                op = 1
-            
             elif char == "+":
-                ans += op * num
+                result += num * op
                 num = 0
                 op = 1
-            
             elif char == "-":
-                ans += op * num
+                result += num * op
                 num = 0
                 op = -1
+            elif char == "(":
+                stack.extend([result, op])
+                num = 0
+                result = 0
+                op = 1
+            elif char == ")":
+                result += num * op
+                prev_op, prev_res = stack.pop(), stack.pop()
+                result = prev_res + prev_op * result
+                num = 0
+                op = 1
             else:
                 num = num * 10 + int(char)
         
-        return ans + num * op
-
-        
+        return result + num * op
