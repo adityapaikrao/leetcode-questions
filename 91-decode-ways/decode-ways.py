a8ab4a03bd9eaeb645e21ab5924daeb299d29945
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-07-27 16:46:19 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.25%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -5,29 +5,39 @@ class Solution:
         
 
         """
-        memo = {}
-        def count_ways(index: int) -> int:
-            if index in memo:
-                return memo[index]
+        # memo = {}
+        # def count_ways(index: int) -> int:
+        #     if index in memo:
+        #         return memo[index]
             
-            # Base Case
-            if index == len(s):
-                # reached end of string 1 way to decode
-                return 1 
-            if int(s[index]) == 0:
-                # no way to decode 
-                return 0
+        #     # Base Case
+        #     if index == len(s):
+        #         # reached end of string 1 way to decode
+        #         return 1 
+        #     if int(s[index]) == 0:
+        #         # no way to decode 
+        #         return 0
 
-            num_ways = 0
-            # 1 digit way 
-            num_ways += count_ways(index + 1)
-            if index + 1 < len(s) and (int(s[index]) * 10 + int(s[index + 1])) <= 26:
-                num_ways += count_ways(index + 2)
-            
-            memo[index] = num_ways
-            return num_ways
+        #     num_ways = 0
+        #     # 1 digit way 
+        #     num_ways += count_ways(index + 1)
+        #     if index + 1 < len(s) and (int(s[index]) * 10 + int(s[index + 1])) <= 26:
+        #         num_ways += count_ways(index + 2)
+        #     memo[index] = num_ways
+        #     return num_ways
         
-        return count_ways(0)
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
+
+        for i in range(len(s) - 1, -1, -1):
+            if int(s[i]) == 0:
+                dp[i] = 0
+                continue
+            dp[i] += dp[i + 1]
+            if i + 1 < len(s) and (int(s[i]) * 10 + int(s[i + 1])) <= 26:
+                dp[i] += dp[i + 2]
+        
+        return dp[0]
 
 
             
