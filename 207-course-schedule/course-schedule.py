aeddfb02953e63c4f7aac23f1be5562daec57fb5
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-09-24 15:44:15 -0400

    Time: 8 ms (21.85%) | Memory: 20.4 MB (63.20%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,22 +1,24 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        n = numCourses
-        adj = [[] for _ in range(n)]
-        for curr, prev in prerequisites:
-            adj[prev].append(curr)
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        in_degree = [0] * numCourses
+        for course, prereq in prerequisites:
+            adj[prereq].append(course)
+            in_degree[course] += 1
+        q = deque()
+        for course in range(numCourses):
+            if in_degree[course] == 0: q.append(course)
         
-        visited = [0] * n
-        def dfs(node: int) -> None:
-            visited[node] = 1
-            for nbr in adj[node]:
-                if visited[nbr] == 1: return False
-                if visited[nbr] != 2: 
-                    if not dfs(nbr): return False
-            visited[node] = 2
-            return True
+        if not q: return False
 
-        for course in range(n):
-            if visited[course] == 0:
-                if not dfs(course): return False
-        return True
+        numNodes = 0
+        while q:
+           curr = q.popleft()
+           numNodes += 1
+           for nbr in adj[curr]:
+                in_degree[nbr] -= 1
+                if in_degree[nbr] == 0:
+                    q.append(nbr)
+
+        return numNodes == numCourses
         
