d3ba91d6778788afe9b4f32358edf4a041657eb9
Aditya Paikrao <58440375+adityapaikrao@users.noreply.github.com>
2026-09-24 22:41:37 -0400

    Time: 242 ms (60.63%) | Memory: 21.7 MB (37.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n, m = len(grid), len(grid[0])
-
-        def dfs(row: int, col: int) -> None:
-            grid[row][col] = "0" # mark as visited
-            
-            for row_off, col_off in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
-                new_row = row + row_off
-                new_col = col + col_off
-                if 0 <= new_row < n and 0 <= new_col < m and grid[new_row][new_col] == "1":
-                    grid[row][col] = "0" # mark as visited
-                    dfs(new_row, new_col)
-        
         num_islands = 0
+
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1":
-                    grid[i][j] = "0" # mark as visited
-                    dfs(i, j)
+                if grid[i][j] == "1": # not yet visited
                     num_islands += 1
+
+                    q = deque([(i, j)])
+                    grid[i][j] = "0"
+                    while q:
+                        curr_i, curr_j = q.popleft()
+                        for offset_i, offset_j in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                            new_i, new_j = offset_i + curr_i, offset_j + curr_j
+                            if 0 <= new_i < n and 0 <= new_j < m and grid[new_i][new_j] == "1":
+                                grid[new_i][new_j] = "0" # mark as visited
+                                q.append((new_i, new_j))
+            
         return num_islands
